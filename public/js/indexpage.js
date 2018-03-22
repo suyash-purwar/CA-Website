bdf91f38f3b84188199fdfd16f587754e8ad62bc
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-03-22 23:54:58 +0530

    Added logo, chnaged carousel images and completed nav for large screens

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -13,6 +13,52 @@ window.sr = ScrollReveal({reset: true});
    setInterval(next, 10000);
 })();
 
+(function() {
+   $('.dropdown-button').dropdown({
+      stopPropagating: false,
+      belowOrigin: true,
+      constrainWidth: false,
+      inDuration: 200
+   })
+
+   $('.services-subsection-1').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    });
+
+    $('.services-subsection-2').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    });
+
+    $('.services-subsection-3').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    });
+})()
+
+// For search modal
+$('#modal1').modal({
+  endingTop: '20%',
+  inDuration: 500,
+  outDuration: 300
+});
+
 // Function for showing thw navbar after some scrolling
 window.onscroll = function() {
    navbarViewEngine()
@@ -20,8 +66,8 @@ window.onscroll = function() {
  
 function navbarViewEngine() {
    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-      $('#large-screen-nav').css('background-color', '#1A2A5A');
+      $('#large-screen-nav').css('display', 'block');
    } else {
-      $('#large-screen-nav').css('background-color', 'transparent');
+      $('#large-screen-nav').css('display', 'flex');
    }
 }

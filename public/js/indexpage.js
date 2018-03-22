6954a3546d957a9ff70da60f8e7206897ae3a343
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-03-22 16:20:00 +0530

    Changed images in carousel and added navigation bar

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -1,14 +1,27 @@
 window.sr = ScrollReveal({reset: true});
 
+(function () {
+   $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+   });
 
-let i = 0;
-$('.carousel.carousel-slider').carousel({
-   fullWidth: true,
-   indicators: true
-});
+   function next() {
+      $('.carousel').carousel('next', 1);
+   }
 
-function next() {
-   $('.carousel').carousel('next', 1);
+   setInterval(next, 10000);
+})();
+
+// Function for showing thw navbar after some scrolling
+window.onscroll = function() {
+   navbarViewEngine()
 }
-
-setInterval(next, 10000)
+ 
+function navbarViewEngine() {
+   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+      $('#large-screen-nav').css('background-color', '#1A2A5A');
+   } else {
+      $('#large-screen-nav').css('background-color', 'transparent');
+   }
+}

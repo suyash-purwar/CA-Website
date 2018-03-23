90fe450d1b015d1482ad7bc2c90b4cf0ed8141a6
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-03-23 15:46:25 +0530

    Made nav for small and medium sized screens and updated nav for large screens

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -24,31 +24,31 @@ window.sr = ScrollReveal({reset: true});
    $('.services-subsection-1').dropdown({
       inDuration: 300,
       outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      constrain_width: false,
+      hover: true,
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5,
+      belowOrigin: true,
+      alignment: 'right'
     });
 
     $('.services-subsection-2').dropdown({
       inDuration: 300,
       outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      constrain_width: false,
+      hover: true,
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5,
+      belowOrigin: true,
+      alignment: 'right'
     });
 
     $('.services-subsection-3').dropdown({
       inDuration: 300,
       outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: ($('.dropdown-content').width()*3)/2.5 + 5, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      constrain_width: false, 
+      hover: true,
+      gutter: ($('.dropdown-content').width()*3)/2.5 + 5,
+      belowOrigin: true, 
+      alignment: 'right' 
     });
 })()
 
@@ -59,15 +59,31 @@ $('#modal1').modal({
   outDuration: 300
 });
 
+// For sidenav
+$('.button-collapse').sideNav({
+  menuWidth: 300,
+  edge: 'right',
+  draggable: true
+});
+
 // Function for showing thw navbar after some scrolling
 window.onscroll = function() {
-   navbarViewEngine()
+   navbarViewEngineForLargeScreens();
+   navbarViewEngineForSmallScreens();
 }
  
-function navbarViewEngine() {
+function navbarViewEngineForLargeScreens() {
    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
       $('#large-screen-nav').css('display', 'block');
    } else {
       $('#large-screen-nav').css('display', 'none');
    }
+}
+
+function navbarViewEngineForSmallScreens() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    $('#small-screen-nav').css('background-color', 'rgb(22, 34, 49)');
+  } else {
+    $('#small-screen-nav').css('background-color', 'transparent');
+  }
 }
